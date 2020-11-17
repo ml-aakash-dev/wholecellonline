57b4c252ec1eeab0c6e1b2816421ced3451548b4
ml-aakash-dev <ml.aakash.dev@gmail.com>
2020-11-18 02:13:09 +0500

    finalized

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Main from './components/main/Main'
-import Reviews from './components/body/Reviews'
-import Testimonials from './components/body/testimonials/AllTestimonials'
+// import Reviews from './components/body/Reviews'
+// import Testimonials from './components/body/testimonials/AllTestimonials'
 
 function App() {
   return (
-    
-    <Router>
-    <Switch>
-      <Route exact path="/" component={Main}/>
-      <Route exact path="/reviews" component={Reviews}/>
-      <Route exact path="/testimonials" component={Testimonials}/>
-    </Switch>
-  </Router>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main}/>
+          {/* <Route exact path="/reviews" component={Reviews}/>
+          <Route exact path="/testimonials" component={Testimonials}/> */}
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
