5da3dc517a52405d5367f027fb16ad73465a94f4
ml-aakash-dev <ml.aakash.dev@gmail.com>
2021-04-05 22:18:56 +0500

    numbers

--- src/components/body/Numbers.js
+++ src/components/body/Numbers.js
@@ -58,17 +58,17 @@ class Numbers extends Component {
                         <Row className="counters">
                             <Col xs={12} sm={6} xl={3} className="background-l col">
                             {/* <div className="counter background-10-light" data-target="358022">0</div> */}
-                                <div className="counter background-10-light">400629</div>
+                                <div className="counter background-10-light">409787</div>
                                 <h3>Satisfied Customers</h3>
                             </Col>
                             <Col xs={12} sm={6} xl={3} className="background-d col">
                             {/* <div className="counter background-10-light" data-target="24442">0</div> */}
-                                <div className="counter background-10-light">27569</div>
+                                <div className="counter background-10-light">28184</div>
                                 <h3>Succesful Repairs</h3>
                             </Col>
                             <Col xs={12} sm={6} xl={3} className="background-l col">
                             {/* <div className="counter background-10-light" data-target="6656">0</div> */}
-                                <div className="counter background-10-light">10378</div>
+                                <div className="counter background-10-light">10507</div>
                                 {/* <h3>5 Star Google Reviews</h3> */}
                                 <h3>
                                 <span className="fa fa-star checked"></span>
@@ -80,7 +80,7 @@ class Numbers extends Component {
                             </Col>
                             <Col xs={12} sm={6} xl={3} className="background-d col">
                             {/* <div className="counter background-10-light" data-target="6656">0</div> */}
-                                <div className="counter background-10-light">98356</div>
+                                <div className="counter background-10-light">102509</div>
                                 <h3>New Phone Sales</h3>
                             </Col>
                         </Row>
